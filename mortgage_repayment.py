60e3c9371f182eb9b04d20b638558ddc82708941
huytu <huytuph@gmail.com>
2021-10-12 22:52:01 +1100

    update

--- mortgage_repayment.py
+++ mortgage_repayment.py
@@ -7,30 +7,30 @@ i = float(input("Interest Rate p.a: ")) / 100 # Interest rate
 t = int(input("Term: ")) # Duration of the loan in years
 n = int(input("Number of repayments per year: ")) # number of repayments p.a
 
-r_bal = p * -1 # remaining Balance
+bal = p * -1 # remaining Balance
 r_n = n * t # remaining Number of payments
 
-i_f = i / n # interest Rate per repayment
+i_n = i / r_n # interest Rate per repayment
 
-i_charged = r_bal * i_f # interest charged per repayment
-p_pay = r_bal / r_n # Principal payable per repayment
+i_charged = bal * i_n # interest charged per repayment
+p_pay = bal / r_n # Principal payable per repayment
 
 i_total = 0 # total Interest paid
 n_total = 0 # total Number of payments
 
-while r_bal <= 0:
+while bal < 0:
     i_total -= i_charged
     n_total += 1
     r_n -= 1
-    r_bal -= p_pay
+    bal -= p_pay
            
-y = n_total // n # years to pay off
-m = n_total % n # remaining months
+y = n_total // n # years
+m = n_total % n # months
 
 print(f"""
 ===========================================
 total Interest Paid: {i_total}
 total Number of repayments made: {n_total}
 Paid off in: {y} years and {m} months
-current balance: {r_bal}
+current balance: {bal}
 """)
